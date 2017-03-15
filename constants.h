410c772ee6c4642759233e02c45c4663d9f0d874
jalonso <juan.alonso@tecnilogica.com>
2017-03-15 20:59:42 +0100

    First attempts to establish a WiFi connection. File with all the secret constants. Code optimized for NodeMCU.

---
+++ constants.h
@@ -0,0 +1,11 @@
+//Spotify data
+char* spotifyAcc = "";
+
+//Bose data
+char* boseIP = "";
+int bosePort = 8090;
+
+//WiFi data
+const char* ssid = "";
+const char* password = "";
+
--- musicbox.ino
+++ musicbox.ino
@@ -1,16 +1,27 @@
+#include <ESP8266WiFi.h>
+#include "constants.h"
 
 //Rotary pins
-int encPinA = 2;
-int encPinB = 3;
+int encPinA = D1;
+int encPinB = D2;
 
 //Delay for unsupervised stream change
-int strChangeDelay = 3000;
+int strChangeDelay = 750;
 
-//Stream names
-char* streams[] = {"primero", "segundo", "tercero",
-                   "cuarto", "quinto", "sexto",
-                   "septimo", "octavo", "noveno"
-                  };
+//Spotify streams
+char* streams[] = {
+  "spotify:station:user:1117954296:cluster:3uOCouLFR4bVx0XeiQJSbl", //DailyMx 1
+  "spotify:station:user:1117954296:cluster:69lxxQvsfAIoQbB20bEPFC", //DailyMx 2
+  "spotify:station:user:1117954296:cluster:1G1mX30GpUJqOr1QU2eBSs", //DailyMx 3
+  "spotify:station:user:1117954296:cluster:4m2iq2WEoxSFvOZd130HIQ", //DailyMx 4
+  "spotify:station:user:1117954296:cluster:5VTWoYYizcOY3uIKnxeCGI", //DailyMx 5
+  "spotify:station:user:1117954296:cluster:4lianjyuR1tqf6oUX8kjrZ", //DailyMx 6
+  "spotify:user:1117954296:playlist:53cVQEQs3AJg2apfGOJiSG",        //770
+  "spotify:user:1117954296:playlist:4L4k1nHskd2lAl4NVUKBru",        //Descubrimientos
+  "spotify:user:1117954296:playlist:2j2qjETKivkmOpbYgrToUT",        //... pero no tanto
+  "spotify:artist:1G1mX30GpUJqOr1QU2eBSs",                          //Radio - Yazoo
+  "spotify:user:aegisthus:playlist:1vY5xwZqNERs6V2aslcZWv",         //Barbudos
+};
 
 //Utility
 int strSize = sizeof(streams) / sizeof(char*);
@@ -28,13 +39,25 @@ boolean countdown = false;
 
 void setup() {
 
-  Serial.begin (9600);
+  Serial.begin (19200);
 
-  pinMode(encPinA, INPUT);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(encPinA, INPUT_PULLUP);
   digitalWrite(encPinA, HIGH);
   lastEncPinAVal = digitalRead(encPinA);
 
-  pinMode(encPinB, INPUT);
+  pinMode(encPinB, INPUT_PULLUP);
   digitalWrite(encPinB, HIGH);
 
 }
@@ -43,13 +66,13 @@ void setup() {
 
 void loop() {
 
-  if(countdown) {
-    if (countdownTime <= millis()){
+  if (countdown) {
+    if (countdownTime <= millis()) {
       countdown = false;
       //We want to change the stream when the timer expires,
       //unles the rotary is in the same position we started
       if (strIndex != playingStrIndex) {
-        Serial.println("***");
+        sendStreamChange();
         playingStrIndex = strIndex;
       }
     }
@@ -76,8 +99,9 @@ void loop() {
 
       //We change the stream only if the strIndex changes
       if (strIndex != lastStrIndex) {
+        Serial.print("[");
         Serial.print(strIndex);
-        Serial.print(": ");
+        Serial.print("] ");
         Serial.println(streams[strIndex]);
         lastStrIndex = strIndex;
         countdownTime = millis() + strChangeDelay;
@@ -87,6 +111,42 @@ void loop() {
     }
     lastEncPinAVal = encPinAVal;
   }
+}
+
+
+void sendStreamChange() {
+  WiFiClient client;
+
+  if (!client.connect(boseIP, bosePort)) {
+    Serial.println("connection failed");
+    Serial.println("wait 5 sec...");
+    delay(5000);
+    return;
+  }
+
+  String boseURL = "/now_playing";
+  Serial.print("Requesting URL: ");
+  Serial.println(boseURL);
+
+  // This will send the request to the server
+  client.print(String("GET ") + boseURL + " HTTP/1.1\r\n" +
+               "Host: " + boseIP + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
 }
 
 
